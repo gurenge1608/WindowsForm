b8193bce4d131707541a9b4356e75fe77cbd5b04
Kentinyhippoo <93818026+Kentinyhippoo@users.noreply.github.com>
2021-11-29 14:16:35 +0700

    UI

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -40,21 +40,22 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(60, 111);
+            this.button1.Location = new System.Drawing.Point(36, 27);
             this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(361, 46);
+            this.button1.Size = new System.Drawing.Size(202, 28);
             this.button1.TabIndex = 0;
             this.button1.Text = "Cập nhật thông tin cá nhân";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(474, 111);
+            this.button2.Location = new System.Drawing.Point(254, 27);
             this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(347, 46);
+            this.button2.Size = new System.Drawing.Size(199, 28);
             this.button2.TabIndex = 1;
             this.button2.Text = "Cập nhật thông tin bài báo đang được nộp";
             this.button2.UseVisualStyleBackColor = true;
@@ -63,9 +64,9 @@
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(60, 218);
+            this.button3.Location = new System.Drawing.Point(254, 74);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(361, 47);
+            this.button3.Size = new System.Drawing.Size(199, 47);
             this.button3.TabIndex = 2;
             this.button3.Text = "Thông tin các tác giả của một bài báo. ";
             this.button3.UseVisualStyleBackColor = true;
@@ -73,9 +74,9 @@
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(474, 218);
+            this.button4.Location = new System.Drawing.Point(38, 74);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(347, 47);
+            this.button4.Size = new System.Drawing.Size(200, 26);
             this.button4.TabIndex = 3;
             this.button4.Text = "Trạng thái của một bài báo";
             this.button4.UseVisualStyleBackColor = true;
@@ -83,9 +84,9 @@
             // button5
             // 
             this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(60, 347);
+            this.button5.Location = new System.Drawing.Point(38, 127);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(361, 47);
+            this.button5.Size = new System.Drawing.Size(200, 48);
             this.button5.TabIndex = 4;
             this.button5.Text = "Kết quả phản biện của một bài báo";
             this.button5.UseVisualStyleBackColor = true;
@@ -93,9 +94,9 @@
             // button6
             // 
             this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(264, 473);
+            this.button6.Location = new System.Drawing.Point(38, 199);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(361, 59);
+            this.button6.Size = new System.Drawing.Size(200, 51);
             this.button6.TabIndex = 5;
             this.button6.Text = "Xem danh sách các bài báo";
             this.button6.UseVisualStyleBackColor = true;
@@ -103,9 +104,9 @@
             // button10
             // 
             this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button10.Location = new System.Drawing.Point(474, 347);
+            this.button10.Location = new System.Drawing.Point(254, 143);
             this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(347, 47);
+            this.button10.Size = new System.Drawing.Size(199, 50);
             this.button10.TabIndex = 9;
             this.button10.Text = "Xem tổng số bài báo";
             this.button10.UseVisualStyleBackColor = true;
@@ -115,7 +116,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(863, 649);
+            this.ClientSize = new System.Drawing.Size(482, 274);
             this.Controls.Add(this.button10);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
